5e434d2156ba40f0263f3cb3d5cadae29ff0bc71
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-24 15:32:44 -0400

    Show error to user if any

--- src/index.js
+++ src/index.js
@@ -7,18 +7,26 @@ class App extends React.Component {
 	constructor(props){
 		super(props);
 
-		this.state = {lat: null};
+		this.state = {lat: null, errMessage:''};
 
 		window.navigator.geolocation.getCurrentPosition(
-		(position) => {
-			this.setState({lat: position.coords.latitude});
-		},
-		(err) => console.log(err)
-		);
+			(position) => {
+				this.setState({lat: position.coords.latitude});
+			},
+			(err) => {
+				this.setState({errMessage: err.message});
+			}
+			);
 	}
 
 	render(){
-		return <div>{Latitude: {this.state.lat}}</div>;
+		return (
+			<div>
+			<div>"Latitude: " {this.state.lat}</div>
+			<br />
+			<div>"Error: " {this.state.errMessage}</div>
+			</div>
+			);
 	}
 }
 
