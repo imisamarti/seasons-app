884c027413ebc2b65b23b538b86a1124013cdbc7
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-24 19:41:59 -0400

    Add icons from semantic UI

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,11 +12,16 @@ const getSeason = (lat,month) =>{
 const SeasonDisplay = props => {
 	const season = getSeason(props.lat,
 		new Date().getMonth());
-	const text = season==="winter" ? "Burr, it's winter, lets snuggle" : "It's summertime, let's hit the beach";
+	const text = season ==="winter" ? "Burr, it's winter, lets snuggle" : "It's summertime, let's hit the beach";
+	const icon = season ==='winter'? 'snowflake' : 'sun';
 
 	return(
 		<div>
+		<i className={`${icon} icon`}></i>
+		<h1>
 		{text}
+		</h1>
+		<i className={`${icon} icon`}></i>
 		</div>
 		);
 };
