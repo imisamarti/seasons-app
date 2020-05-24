c553c861bff9a96e8fc1791e98b62a8b6d5a8c3c
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-24 14:59:18 -0400

    Create class App

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 
 import SeasonDisplay from "./SeasonDisplay"
 
-const App = () => {
-
-	window.navigator.geolocation.getCurrentPosition(
+class App extends React.Component {
+	render(){
+		window.navigator.geolocation.getCurrentPosition(
 		(position) => console.log(position),
 		(err) => console.log(err)
 		);
-
-	return (
-		<div>
-		{SeasonDisplay()}
-		</div>
-		);
-};
+		
+		return <div>{SeasonDisplay()}</div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
