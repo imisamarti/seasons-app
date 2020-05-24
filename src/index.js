5dec285fcb3f9a2ebf0bb1687d2b6d4d4cf4dd03
IsadoraMartinez <isadoramartinez123@gmail.com>
2020-05-24 15:09:29 -0400

    Create constructor

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay"
 
 class App extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
 	render(){
 		window.navigator.geolocation.getCurrentPosition(
 		(position) => console.log(position),
 		(err) => console.log(err)
 		);
-		
+
 		return <div>{SeasonDisplay()}</div>;
 	}
 }
